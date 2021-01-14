209be96c04a2b43076dcd621a42b1959340c7211
Ben Jackson <b@benjackson.email>
2021-01-14 16:36:04 +1100

    update charts

---
+++ helm-update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+index-fastly () {
+    pushd charts
+    helm package fastly-controller
+    helm repo index .
+    popd
+}
+
+case $1 in
+  index)
+    index-fastly
+    ;;
+  template)
+    helm template charts/fastly-controller -f charts/fastly-controller/values.yaml \
+      --set fastly.apiToken=${FASTLY_API_TOKEN} \
+      --set fastly.tlsConfigID=${FASTLY_PLATFORM_TLS_CONFIGURATION_ID} \
+      --set fastly.clusterName=${CLUSTER_NAME}
+    ;;
+  install)
+    helm repo add fastly-controller https://raw.githubusercontent.com/amazeeio/fastly-controller/master/charts
+    helm upgrade --install -n fastly-controller fastly-controller fastly-controller/fastly-controller \
+      --set fastly.apiToken=${FASTLY_API_TOKEN} \
+      --set fastly.tlsConfigID=${FASTLY_PLATFORM_TLS_CONFIGURATION_ID} \
+      --set fastly.clusterName=${CLUSTER_NAME}
+    ;;
+  install-tgz)
+    helm upgrade --install -n fastly-controller fastly-controller  charts/fastly-controller-0.0.2.tgz  \
+      --set fastly.apiToken=${FASTLY_API_TOKEN} \
+      --set fastly.tlsConfigID=${FASTLY_PLATFORM_TLS_CONFIGURATION_ID} \
+      --set fastly.clusterName=${CLUSTER_NAME}
+    ;;
+  *)
+    echo "nothing"
+    ;;
+esac
