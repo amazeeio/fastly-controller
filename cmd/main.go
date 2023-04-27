0c6f6b6cf9d725dfd9e86241a624b980f22201ae
Marco Cadetg <cadetg@gmail.com>
2023-04-27 08:47:23 +0200

    Update cmd/main.go
    revert leader election id
    
    Co-authored-by: Tobi Nehrlich <tobi@anothertobi.io>

--- cmd/main.go
+++ cmd/main.go
@@ -112,7 +112,7 @@ func main() {
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "5153988a.amazee.io",
+		LeaderElectionID:       "lahc7eig.amazee.io",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
